54435bfc360fd00a6abb6a6ba158b81f0077b135
AmanGr <alphagr110@gmail.com>
2021-07-25 04:37:55 +0300

    some change have been made

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ const kBottomContainerHeight = 80.0;
 const kActiveCardColor = Color(0xff1A1A28);
 const kInactiveCardColor = Color(0XFF111328);
 const kBottomContainerColor = Color(0XFFEB1555);
-//those are const
+//those are constant
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
