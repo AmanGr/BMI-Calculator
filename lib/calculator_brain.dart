360d8687b666d7ca5e0a6e2b2c7f8442253b0107
AmanGr <alphagr110@gmail.com>
2021-08-04 02:24:59 +0300

    some changes have been takes placed

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -7,15 +7,15 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
-  double? _bmi;
+  dynamic  _bmi;
 
   String calculateBMI() {
     _bmi = weight / pow(height / 100, 2);
-    return _bmi!.toStringAsFixed(1);
+    return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi >= 25) {
+    if (_bmi! >= 25) {
       return "OverWeight";
     } else if (_bmi > 18.5) {
       return "Normal";
